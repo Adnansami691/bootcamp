a06767ddadaa3e58a5de02a28c2e08e006bb7eac
Adnansami691 <samiadnn691@gmail.com>
2025-02-11 03:05:48 +0600

    nested if else

--- second.cpp
+++ second.cpp
@@ -1,12 +1,27 @@
 #include<bits/c++io.h>
 #include<iostream>
-
+using namespace std;
 main(){
-    int x;
-    std::cin >> x;
-    std::cout << x;
-    
+    int tk = 30;
 
+    if (tk >= 200){
+        cout << "burger";
+    }
+    else {
+        cout << "no burger!! \n";
+        cout << "hello";
+    }
+
+
+    int y = 500;
+    if (y >= 0) {
+        if(y == 500){
+            cout << "\nburger + pizza";
+        }
+        else {
+            cout << "Nothing";
+        }
+    }
 
 
     return 0;
