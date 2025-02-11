2d75f8ac9733307c87988a413613fdeaa83922f8
Adnansami691 <samiadnn691@gmail.com>
2025-02-11 19:05:53 +0600

    prlbm for loop fixed

--- third.cpp
+++ third.cpp
@@ -4,14 +4,11 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    if (n % 2 == 0){
-    for (int i=1; i <= n; i++) {
+    for (int i=1;i <= n; i++){
+        if(i % 2 == 0)
         cout << i << "-even\n";
-    }
-    }
-    else {
-        for(int i=1;i <= n; i++)
-        cout << i << "-odd\n"; 
+        else
+        cout << i << "-odd\n";
     }
 
     return 0;
