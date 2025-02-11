15349c261564b2b68040e9ce8b54cc75a2fd50f2
Adnansami691 <samiadnn691@gmail.com>
2025-02-11 19:01:19 +0600

    prblm for loop

--- third.cpp
+++ third.cpp
@@ -4,10 +4,15 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
+    if (n % 2 == 0){
     for (int i=1; i <= n; i++) {
-        cout << "hello\n";
+        cout << i << "-even\n";
     }
-
+    }
+    else {
+        for(int i=1;i <= n; i++)
+        cout << i << "-odd\n"; 
+    }
 
     return 0;
 }
