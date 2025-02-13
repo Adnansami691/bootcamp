9cfb2b0b1f542fd0f26ad5522a6da0231f12754f
Adnansami691 <samiadnn691@gmail.com>
2025-02-14 02:09:45 +0600

    full tree structure

--- tree.cpp
+++ tree.cpp
@@ -3,13 +3,26 @@ using namespace std;
 
 
 int main(){
+    int star = 1;
     for (int i=0;i<=5;i++ ){
+   
+        for (int j= 1;j<= star ;j++){
         cout << "*";
-    
+    }
         cout << endl;
+        star++;
     }
 
 
 
     return 0;
 }
+
+
+
+
+// * -> loop 1 time
+// ** -> loop 2 times
+// *** -> loop 3 times
+// **** -> loop 4 times
+// ***** -> loop 5 times
