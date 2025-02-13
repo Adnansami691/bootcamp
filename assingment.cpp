6493ea7072b77c5a98e341944f886ebf07c70874
Adnansami691 <samiadnn691@gmail.com>
2025-02-13 22:00:09 +0600

    assingment

---
+++ assingment.cpp
@@ -0,0 +1,36 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int N;
+    cin >> N;
+    for (int i=1;i <=N;i++){
+        if (i%2 == 0)
+            int sum = i + i;
+        else if (i%3 == 0)
+            int sum = i + i;
+    }
+    return 0;
+}
