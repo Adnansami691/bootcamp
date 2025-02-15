357fb5f3998edea73b1f59e522fe0af8898e7204
Adnansami691 <samiadnn691@gmail.com>
2025-02-15 19:31:59 +0600

    errorgit add .

--- tree_pattern.cpp
+++ tree_pattern.cpp
@@ -1,49 +1,29 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-void printTree(int trunkWidth) {
-    // Print the leaves of the tree (always 7 lines)
-    for (int i = 1; i <= 7; i++) {
-        // Print spaces to center the leaves
-        for (int j = 0; j < 7 - i; j++) {
-            cout << " ";
-        }
-        // Print the leaves
-        for (int j = 0; j < 2 * i - 1; j++) {
-            cout << "*";
-        }
-        cout << endl;
-    }
-
-    // Print the trunk of the tree (always 5 lines tall)
-    for (int i = 0; i < 5; i++) {
-        // Print spaces to center the trunk
-        for (int j = 0; j < 7 - (trunkWidth / 2 + 1); j++) {
-            cout << " ";
-        }
-        // Print the trunk
-        for (int j = 0; j < trunkWidth; j++) {
-            cout << "*";
-        }
-        cout << endl;
-    }
-}
-
 int main() {
-    int trunkWidth;
-
-    // Ask the user for the width of the trunk
-    cout << "Enter the width of the trunk (odd number): ";
-    cin >> trunkWidth;
-
-    // Check if the input is valid (positive integer)
-    if (trunkWidth <= 0) {
-        cout << "Please enter a positive integer." << endl;
-        return 1;
+    for (int i= 0;i < 9; i++){
+        for (int j=0; j < 9-i-1; j++){
+            cout <<" ";
+        }
+        for (int k=0;k < i*2+1;k++){
+            cout <<"*";
+        }
+        cout<<endl;
     }
-
-    // Print the tree
-    printTree(trunkWidth);
-
+    for (int i= 0; i< 3; i++){
+        for (int i =0;i < 3;i++){
+            cout<<" ";
+        }
+        for(int i=0;i < 3;i++){
+            cout<< "*";
+        }
+        cout<< endl;
+    }
+    
+    
+    
     return 0;
+
+
 }
