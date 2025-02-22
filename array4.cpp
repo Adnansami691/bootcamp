56d1289374ac3f8ea8b5b364d3a5537490def406
Adnansami691 <samiadnn691@gmail.com>
2025-02-22 22:22:25 +0600

    solvedgit add .

--- array4.cpp
+++ array4.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main() {
     int N;
-    cin >> N;  // Read the size of the array
-    
-    vector<int> A(N);  // Declare a vector of size N
-    
-    for (int i = 0; i < N; i++) {
-        cin >> A[i];  // Read the array elements
+    cin >> N;
+    int A[N];
+    for (int i=0;i<N;i++){
+        cin >> A[i];
     }
-    
-    int X, V;
-    cin >> X >> V;  // Read X (index) and V (new value)
-    
-    // Update the X'th index with V
+    int X,V;
+    cin >> X >> V;
     A[X] = V;
-    
-    // Print the array in reverse order
-    for (int i = N - 1; i >= 0; i--) {
+    for (int i = N -1;i>=0;i--){
         cout << A[i] << " ";
     }
-    
+
     return 0;
 }
