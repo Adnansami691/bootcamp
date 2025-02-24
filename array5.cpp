a33566071a455fcf5d45eba561cb47a2459d55c4
Adnansami691 <samiadnn691@gmail.com>
2025-02-24 22:34:27 +0600

    solve foundgit add .

--- array5.cpp
+++ array5.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    string s;
+    string s = "hello";
     string A;
     string a;
-    cin >> s;
     
-    int index =0;
-    int asciivalue;
-    for(int i=0;i>s[];i++){
-        if(i >=65 && i<=90){
-            A = i;
-        }
-        else {
-            a = i;
-        }
-        index++; 
-    }
-    cout << a << " ";
-    cout << A << " ";
+    cout << s.length();
+    // int index =0;
+    // int asciivalue;
+    // // for(int i=0;i>s[];i++){
+    // //     if(i >=65 && i<=90){
+    // //         A = i;
+    //     }
+    //     else {
+    //         a = i;
+    //     }
+    //     index++; 
+    // // }
+    // cout << a << " ";
+    // cout << A << " ";
     
     return 0;
 }
