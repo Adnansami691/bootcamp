0784fff120c6f1055b6f40a9fc3ad113d8f5a4d6
Adnansami691 <samiadnn691@gmail.com>
2025-02-24 22:22:30 +0600

    problem 3git add .

--- array5.cpp
+++ array5.cpp
@@ -1,10 +1,25 @@
 #include<iostream>
 using namespace std;
-
-int main(){
-    string a ="hello ";
-    string b ="world!!";
-    cout << a << b;
-
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    string s;
+    string A;
+    string a;
+    cin >> s;
+    
+    int index =0;
+    int asciivalue;
+    for(int i=0;i>s[];i++){
+        if(i >=65 && i<=90){
+            A = i;
+        }
+        else {
+            a = i;
+        }
+        index++; 
+    }
+    cout << a << " ";
+    cout << A << " ";
+    
     return 0;
 }
