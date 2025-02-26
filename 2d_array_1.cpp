dccc7cc4adf0e96562dfc6352d8de3c73349cb8c
Adnansami691 <samiadnn691@gmail.com>
2025-02-26 22:29:26 +0600

    2d array matrixgit add .!

---
+++ 2d_array_1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int grid[n+1][n+1];
+    for(int row=1;row<=n;row++){
+        for(int col=1;col<=n;col++){
+            cin >> grid[row][col];
+        }
+    }
+    bool idm = true;
+
+    for(int row =1;row<=n;row++){
+        for(int col =1;col <= n;col++){
+            if(row == col && grid[row][col] == 1){
+                idm = true;
+            }
+            else{
+                idm = false;
+            }
+            if(row != col){
+                if(grid[row][col] == 0){
+    
+                }
+                else{
+                    idm = false;
+                }
+            
+        }
+            }
+    }
+    if(idm){
+        cout << "hola!!";    
+    }
+    else{
+        cout << "Bonjuor!!";
+    }
+    return 0;
+}
